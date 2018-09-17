4f16e0b29d64e43e088e18733139cbfc0df08bb1
Masa <mkawaba2@u.rochester.edu>
2018-09-17 15:50:24 -0400

    John_Roman
    2nd Page

---
+++ Jonathan_Roman.php
@@ -0,0 +1,38 @@
+<?php include "include/html-top.inc"; ?>
+<body>
+	<?php include "include/nav.inc"; ?>
+	
+	<h1>JONATHAN ROMAN</h1>
+
+	<div class="JRomanPicture">
+		<img src="images/JRomanPic.jpg" alt="JRomanPic" class="Picture">
+	</div>
+		
+	<div class="info">
+		<p>My name is Jonathan Roman. I'm from Tenafly, NJ and was born September 6th, 1997. I currently attend the University of Rochester with a major in Computer Science. MY hobbies include playing video games and basketball, both of which I participate in groups around campus for.
+		</p>
+	</div>
+
+	<div class="info">
+		<p>As far as my academic career used to be, I was very into biology in high school. I would attend science competitions and fairs to present research I had begun through a special science research program in my school (which I credit my acceptance to the University of Rochester to). Since I was still a teenager, I had not really explored many fields up to this point and although the idea of biology excited me, I do not believe I was truly passionate about it. I had always been into video games and technology since I was old enough to operate a screen, so it seemed logical that I would like computer science, but my school did not offer anything in the field, so I entered the University of Rochester with a major in biology. Once I arrived at college, I continued with my track for a year or two before I began taking classes in computer science and instantly fell in love with it. I did not realize how much I would truly enjoy the field but I am extremely happy I will end up being able to complete my major within the 4 years even with some extra classes from other fields under my belt.
+		</p>
+	</div>
+
+	<div class="info">
+		<p>
+		My hobbies slightly changed when I arrived to college. I, for the most part, stopped playing tennis and picked up basketball as my favorite sport. Since then I've tried to play nearly every day, and I'm currently the captain of the men's club basketball team here at the University of Rochester. It's been a very different experience to transition from an individually dependent sport such as tennis to a fully organized team sport like basketball. It's been a learning experience and really has helped me grow as a person in regards to my ability to work well with other people / teammates. My interest in gaming remains mostly unchanged, and I'm still keeping up to date with all the changing information in that industry.
+		</p>
+	</div>
+
+	<div class="info">
+		<p>Throughout high school I would work as a part-time tennis coach, though mostly had very light hours (about 4-5 hours a week). The hours were fairly flexible and I could pick up lessons whenever my local tennis club was teaching them, allowing me to change my schedule regularly as needed. At the time I was on the varsity tennis team so the only prerequisite I needed was to be able to play and have the patience to deal with children aged anywhere from 6-14. This was my first job I received on my own as well, which was different but educational for a 15-year-old me. Over the summer, I also taught at the summer camp which was a lot more demanding, as the hours would often be, besides for certain exceptions, 9am-4pm.
+		</p>
+		<p>
+		I have also worked as a warehouse employee for my dad, I would often be moving and packaging boxes and/or searching for items that needed to be shipped in the warehouse in order to package. Although it was not the most entertaining job in the world, I learned a lot about my dad's company and would be able to spend a lot of time working with my siblings along with my dad.
+		</p>
+	</div>
+	
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="scripts/js/menu-highlighter.js"></script>
+</body>
+</html>
