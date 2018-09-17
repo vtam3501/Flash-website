a006bce5e037c3db5491fd06c036bff9110ef2b2
Masa <mkawaba2@u.rochester.edu>
2018-09-17 17:52:12 -0400

    Hope Edit
    Added h2 and changed spacings

--- hope_barkley.php
+++ hope_barkley.php
@@ -1,19 +1,16 @@
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-	<?php include('./inc/html-top.php'); ?>
-</head>
+<?php include "include/html-top.inc"; ?>
 <body>
-	<?php include('./inc/nav.php'); ?>
-	<div class="ui raised very padded text container segment">
+
+	<?php include "include/nav.inc"; ?>
+
 	<h1>HOPE BARKLEY</h1>
+
 
 	<div class="majors">
 		<div>
 			<img src="images/portrait.jpg" alt = "Hope Barkley">
 		</div>
-		<div class = "ui bulleted list">
+		<div>
 		<ul class="list1">
 			<li class="header"> Areas of study </li>
 			<li> Digital Media Studies </li>
@@ -32,34 +29,51 @@
 	</div>
 
 	<div class="info">
+
 		<p>
-			 Hi, my name is Hope Barkley and I am a digital media studies student at the University of Rochester. I grew up in Tarrytown, NY, a suburb about 30 minutes outside of New York City. I attended Solomon Schechter School of Westchester, a Jewish day school located in Hartsdale, New York. I started gymnastics when I was 7 years-old and was a competetive gymnast throughout my high school career, before deciding not to pursue it in college. Before going to college, I reached USAG level 9 gymnastics and competed on a State and Regional level. During my senior year of high school I applied Early Decision to the University of Rochester and was accepted! I am currently majoring in Digital Media Studies, an interdisciplinary major focussing on technology industries, and hope to pursue a career in design or web development post graduation.
+			 Hi, my name is Hope Barkley and I am a digital media studies student at the University of Rochester. I grew up in Tarrytown, NY, a suburb about 30 minutes outside of New York City. I attended Solomon Schechter School of Westchester, a Jewish day school located in Hartsdale, New York. I started gymnastics when I was 7 years-old and was a competetive gymnast throughout my high school career, before deciding not to pursue it in college. Before going to college, I reached USAG level 9 gymnastics and competed on a State and Regional level. During my senior year of high school I applied Early Decision to the University of Rochester and was accepted! I am currently majoring in Digital Media Studies, an interdisciplinary major focussing on technology industries, and hope to pursue a career in design or web development post graduation. 
 		</p>
+
 	</div>
 
+	<h2>Courses: </h2>
+
 	<div class="info">
+
 		<p>
 			At University of Rochester, I have taken many classes to help advance my skillsets and knowledge. I have completed courses such as DMS 101, DMS 102, DMS 103, and DMS 104 to ensure that I am prepared for upper level DMS classes. Additionally, I am one course away from completion of my computer science minor, and now have HTML, Python and Java skills. I am currently enrolled in many Business courses, such as Finance, and have previously taken multiple Economics and accounting classes.
 		</p>
+
 	</div>
+
+	<h2>Jobs: </h2>
 
 	<div class="info">
+
 	<p>
-		I have had a variety of internships and jobs throughout my life. My first job was as a gymnastics coach at Westchester Gymnastics, where I taught to beginners. I spent the summer of 2016 working as the gymnastics coordinator for a day camp run by the Jewish Community Center of Tarrytown, where I worked with over 300 children every day teaching basic gymnastics skills. In the summer of 2017 following my Freshmen year of college, I worked in the User Experience Design department of Mastercard in NYC, and got the chance to work on a variety of exciting projects such as the redesign of MC's website, apps and conduct multiple research tests. This past summer, I was a graphic design intern for Regeneron, a biotechnology company located in Tarrytown NY. In this position, I helped run and manage many of their social media campaigns, create designs for corporate materials, and learned a great deal about marketing within the heathcare industry.
+		I have had a variety of internships and jobs throughout my life. My first job was as a gymnastics coach at Westchester Gymnastics, where I taught to beginners. I spent the summer of 2016 working as the gymnastics coordinator for a day camp run by the Jewish Community Center of Tarrytown, where I worked with over 300 children every day teaching basic gymnastics skills. In the summer of 2017 following my Freshmen year of college, I worked in the User Experience Design department of Mastercard in NYC, and got the chance to work on a variety of exciting projects such as the redesign of MC's website, apps and conduct multiple research tests. This past summer, I was a graphic design intern for Regeneron, a biotechnology company located in Tarrytown NY. In this position, I helped run and manage many of their social media campaigns, create designs for corporate materials, and learned a great deal about marketing within the heathcare industry.  
 	</p>
+
 	</div>
 
-	<div class="intro">
+	<h2>Teaching Assistant: </h2>
+		
+	<div class="info">
+
 	<p>
-		 Last semester, I was a teaching assistant for Economics 207, an intermediate microeconomics course taught by Professor Rizzo. As part of my responsiblities I graded Problem sets, exams, and help develop the course scope. I really enjoyed this course when I took it and loved the chance to teach other students. This semester I am currently working as a Teaching Assistant for the Computer Science department in an introduction to Python course, CSC 161 taught by Professor Sarkis. I really loved the problem solving aspect of the labs, and so I decided to serve as a lab TA for the course. Weekly, I run two lab sessions for upwards of 50 students and answer questions on their homework assignments.
+		 Last semester, I was a teaching assistant for Economics 207, an intermediate microeconomics course taught by Professor Rizzo. As part of my responsiblities I graded Problem sets, exams, and help develop the course scope. I really enjoyed this course when I took it and loved the chance to teach other students. This semester I am currently working as a Teaching Assistant for the Computer Science department in an introduction to Python course, CSC 161 taught by Professor Sarkis. I really loved the problem solving aspect of the labs, and so I decided to serve as a lab TA for the course. Weekly, I run two lab sessions for upwards of 50 students and answer questions on their homework assignments. 
 	</p>
+
 	</div>
 
+	
 	<footer>
 		<p class="hi"> Hope Barkley 2018 </p>
 	</footer>
+	
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
 
-	</div >
 </body>
 
 
