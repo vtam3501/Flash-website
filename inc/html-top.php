3e34fcfc61ff0be6add6036742e23bee067b16e9
famousclark <clark.famous@gmail.com>
2018-09-17 16:10:41 -0400

    # added html-inc - famous

---
+++ inc/html-top.php
@@ -0,0 +1,6 @@
+<head>
+	<meta charset="utf-8">
+	<title>Hope Barkley</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css">
+			<link rel="stylesheet" href="styles.css">
+</head>
