660aa3c8aef2a030d0826bc861ca01b54c0dfdf7
Famous Clark <clark.famous@gmail.com>
2018-09-19 16:00:42 -0400

    Resolving Conflicts - Famous

--- julia_myers.php
+++ julia_myers.php
@@ -1,13 +1,17 @@
-<?php include "include/html-top.inc"; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<?php include('./inc/html-top.php'); ?>
+</head>
 
 	<body>
-		<div class="container">
+		<?php include('./inc/nav.php'); ?>
+		<div class="ui raised very padded text container segment">
 
 		<header>
-			<h1>JULIA MYERS</h1>
+			<h1>Julia Myers</h1>
 		</header>
 
-		<?php include "include/nav.inc"; ?>
 
 		<img src="images/julia.jpg" alt="Julia"/>
 
@@ -21,12 +25,6 @@
 			<p> I am a junior at the University of Rochester pursuing a major in Digital Media Studies as well as a double minor in Computer Science and Business.  I am originally from Westfield, New Jersey. Use the tabs above to learn more about myself, my courses, and my work experience!</p>
 		</div>
 
-		<h2>About Me:</h2>
-
-		<div class="info">
-			<p> Hi, My name is Julia! I am originally from Westfield, New Jersey but I currently live in Rocehster, New York while attending the University of Rochester.  At UofR I am studying to get a major in Digital Media Studies and a double minor in Computer Science and Business.  I am also a member of the varsity Cross Country and Track and Field Teams.  Outside of classes and athletics, I am a member of the Rochester Design club, the Women in Computing club, and the Varsity Student Athlete Advisory Committee.  I am also a Study Abroad Ambassador.  I have a brother who also attented the University of Rochester, a sister, and a dog.  In my free time I like listening to music, running, and taking pictures.</p>
-		</div>
-
 		<h2>Experience:</h2>
 
 		<div class="info">
@@ -34,7 +32,7 @@
 
 			<p>Also while in high school I worked as a tutor for the National Honors Society.  I tutored English and Math to a freshman student and helped him to study for the SAT.</p>
 
-			<p>At the end of high school I interned with Nordstrom as a Fashion Ambassador on their BP Fashion Board.  As an intern I helped advise the staff on current trends, make decisions about what should be sold in the teen section of Nordstrom, and promote the brand on social media.</p> 
+			<p>At the end of high school I interned with Nordstrom as a Fashion Ambassador on their BP Fashion Board.  As an intern I helped advise the staff on current trends, make decisions about what should be sold in the teen section of Nordstrom, and promote the brand on social media.</p>
 
 
 			<p>While studying abroad in Copenhagen this past spring, I interned at IIH Nordic, an online marketing and data analytics company.  There, I wrote articles for their website, helped to make promotional videos for the company, and helped to plan their events. I learned more about the possible professions in the technology and marketing fields, and gained practical work experience. Those experiences in conversation with my major courses have made me interested in pursuing an online marketing or data analytics career.</p>
@@ -50,6 +48,12 @@
 
 		</div>
 
+		<h2>About Me:</h2>
+
+		<div class="info">
+			<p> Hi, My name is Julia! I am originally from Westfield, New Jersey but I currently live in Rocehster, New York while attending the University of Rochester.  At UofR I am studying to get a major in Digital Media Studies and a double minor in Computer Science and Business.  I am also a member of the varsity Cross Country and Track and Field Teams.  Outside of classes and athletics, I am a member of the Rochester Design club, the Women in Computing club, and the Varsity Student Athlete Advisory Committee.  I am also a Study Abroad Ambassador.  I have a brother who also attented the University of Rochester, a sister, and a dog.  In my free time I like listening to music, running, and taking pictures.</p>
+		</div>
+
 		<footer class="footer">
 			<p>Julia Myers, CSC 174 Advanced Webpage Design and Development</p>
 		</footer>
@@ -58,5 +62,8 @@
 		<script src="js/menu-highlighter.js"></script>
 
 	</div>
+	<script type="text/javascript" src="scripts/nav.js">
+
+	</script>
 </body>
 </html>
