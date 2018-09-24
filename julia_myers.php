fd0565808cb32ed0b4a8281290a2ce4f7427b584
Lingling <lli53@u.rochester.edu>
2018-09-24 17:49:55 -0400

    IA changes on the content

--- julia_myers.php
+++ julia_myers.php
@@ -1,65 +1,68 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<!doctype html>
+
+<html lang="en">
+
 <head>
-	<?php include('./inc/html-top.php'); ?>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/navigation.css">
+	<link rel="stylesheet" type="text/css" href="css/current.css">
+	<title>Julia Myers | Team Tokyo</title>
 </head>
 
-	<body>
-		<?php include('./inc/nav.php'); ?>
-		<div class="ui raised very padded text container segment">
+<body>
+	<header>	
+		<h1>Julia Myers</h1>
+	</header>
 
-		<header>
-			<h1>Julia Myers</h1>
-		</header>
+	<?php include "inc/nav.inc"; ?>
 
+	<article>
 
-		<img src="images/julia.jpg" alt="Julia"/>
+		<h2> Brief Introduction</h2>
 
-		<ul>
-			<li>Junior at the University of Rochester</li>
-			<li>Digital Media Studies, Computer Science, Business</li>
-			<li>Cross Country and Track athlete</li>
-		</ul>
-
-		<div class="info">
-			<p> I am a junior at the University of Rochester pursuing a major in Digital Media Studies as well as a double minor in Computer Science and Business.  I am originally from Westfield, New Jersey. Use the tabs above to learn more about myself, my courses, and my work experience!</p>
-		</div>
-
-		<h2>Experience:</h2>
-
-		<div class="info">
-			<p> Since I was 16 years old, I have worked as a lifeguard and a swim instructor.  I mainly held this job to make money throughout high school and to gain work experience.</p>
-
-			<p>Also while in high school I worked as a tutor for the National Honors Society.  I tutored English and Math to a freshman student and helped him to study for the SAT.</p>
-
-			<p>At the end of high school I interned with Nordstrom as a Fashion Ambassador on their BP Fashion Board.  As an intern I helped advise the staff on current trends, make decisions about what should be sold in the teen section of Nordstrom, and promote the brand on social media.</p>
+			<figure>
+				<img src="images/portrait.jpg" alt = "Hope Barkley">
+				<figcaption>
+					Here is a picture of Hope Barkley
+				</figcaption>
+			</figure>
+			
+			<p> 
+				Hi, My name is Julia! I am originally from Westfield, New Jersey but I currently live in Rocehster, New York while attending the University of Rochester.  At UofR I am studying to get a major in Digital Media Studies and a double minor in Computer Science and Business.  I am also a member of the varsity Cross Country and Track and Field Teams.  Outside of classes and athletics, I am a member of the Rochester Design club, the Women in Computing club, and the Varsity Student Athlete Advisory Committee.  I am also a Study Abroad Ambassador.  I have a brother who also attented the University of Rochester, a sister, and a dog.  In my free time I like listening to music, running, and taking pictures.
+			</p>
 
 
-			<p>While studying abroad in Copenhagen this past spring, I interned at IIH Nordic, an online marketing and data analytics company.  There, I wrote articles for their website, helped to make promotional videos for the company, and helped to plan their events. I learned more about the possible professions in the technology and marketing fields, and gained practical work experience. Those experiences in conversation with my major courses have made me interested in pursuing an online marketing or data analytics career.</p>
-		</div>
+		<h2>Academic Story</h2>
 
-		<h2>Courses:</h2>
-
-		<div class="info">
-			<p> When I started school at the University of Rochester I started taking a lot od technology and design classes.  For my Digital Media Studies major I have taken courses including Programming for Digital Media, Design in the Digital Age, and Photography which have given me a well-rounded understanding of the media industries and the possible career paths within them.  Additionally, for my Computer Science minor, I have taken courses such as aken at the University of Rochester.  Computer Science classes such as Intro to Programming, Web design and Development, and Recreational Graphics.  For my Business minor I am currently taking Economics and Statistics and will go on to take Finance and Marketing classes in future semesters. </p>
+			<p>
+			 	When I started school at the University of Rochester I started taking a lot od technology and design classes.  For my Digital Media Studies major I have taken courses including Programming for Digital Media, Design in the Digital Age, and Photography which have given me a well-rounded understanding of the media industries and the possible career paths within them.  Additionally, for my Computer Science minor, I have taken courses such as aken at the University of Rochester.  Computer Science classes such as Intro to Programming, Web design and Development, and Recreational Graphics.  For my Business minor I am currently taking Economics and Statistics and will go on to take Finance and Marketing classes in future semesters. 
+			</p>
 
 
-			<p>During the spring semester of my sophomore year, I studied abroad in Copenhagen, Denmark.  There, I focused my studies on Communications and Media studies.  While abroad I took Cross Cultural Communicaiton, Digital Media in Marketing, Photojournalism, and Sociology of the Family.  I also had an internship in Copenhagen that you can read about in the experiences page!</p>
+			<p>
+				During the spring semester of my sophomore year, I studied abroad in Copenhagen, Denmark.  There, I focused my studies on Communications and Media studies.  While abroad I took Cross Cultural Communicaiton, Digital Media in Marketing, Photojournalism, and Sociology of the Family.  I also had an internship in Copenhagen that you can read about in the experiences page!
+			</p>
 
-		</div>
 
-		<h2>About Me:</h2>
+		<h2>Experience </h2>
+			
+			<p>
+				While studying abroad in Copenhagen this past spring, I interned at IIH Nordic, an online marketing and data analytics company.  There, I wrote articles for their website, helped to make promotional videos for the company, and helped to plan their events. I learned more about the possible professions in the technology and marketing fields, and gained practical work experience. Those experiences in conversation with my major courses have made me interested in pursuing an online marketing or data analytics career.
+			</p>
 
-		<div class="info">
-			<p> Hi, My name is Julia! I am originally from Westfield, New Jersey but I currently live in Rocehster, New York while attending the University of Rochester.  At UofR I am studying to get a major in Digital Media Studies and a double minor in Computer Science and Business.  I am also a member of the varsity Cross Country and Track and Field Teams.  Outside of classes and athletics, I am a member of the Rochester Design club, the Women in Computing club, and the Varsity Student Athlete Advisory Committee.  I am also a Study Abroad Ambassador.  I have a brother who also attented the University of Rochester, a sister, and a dog.  In my free time I like listening to music, running, and taking pictures.</p>
-		</div>
+
+		<h2>Interesting story of my life</h2>
+
+			<p> 
+				Since I was 16 years old, I have worked as a lifeguard and a swim instructor.  I mainly held this job to make money throughout high school and to gain work experience. Also while in high school I worked as a tutor for the National Honors Society.  I tutored English and Math to a freshman student and helped him to study for the SAT.At the end of high school I interned with Nordstrom as a Fashion Ambassador on their BP Fashion Board.  As an intern I helped advise the staff on current trends, make decisions about what should be sold in the teen section of Nordstrom, and promote the brand on social media.
+			</p>
+
+	</article>
 
 	<footer>
 		Julia myers 2018 | <a href="http://www.csc174.org/assignment01/jmyers/lab_01/"> See my initial website! </a>
 	<footer>
-
-		
-	</div>
 
 	<?php include "inc/scripts.inc"; ?>
 	
