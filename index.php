bc9b21afe5df1484b22554ea5e95b7ad36b5c5a8
Lingling <lli53@u.rochester.edu>
2018-09-24 16:30:14 -0400

    change index

--- index.php
+++ index.php
@@ -1,28 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <title>Hope Barkley</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css">
-    <link rel="stylesheet" href="styles/styles.css">
+  <meta charset="utf-8">
+  <title>Flash Website | Team Tokyo</title>
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/navigation.css">
+  <link rel="stylesheet" type="text/css" href="css/current.css">
+  <link rel="icon" href="img/icon.png">
 </head>
- <body>
 
-    <div class="ui raised very padded text container segment">
-      <div class="ui link list">
-        <ul>
-          <ul>
-      		  	<li><a href="index.php">Home</a></li>
-      		  	<li><a href="hope_barkley.php">Hope Barkley</a></li>
-      		  	<li><a href="Jonathan_Roman.php">Jonathan Roman</a></li>
-      		  	<li><a href="julia_myers.php">Julia Myers</a></li>
-      		</ul>
+<body>
+  <header>
+      <h1>The Flash Website of Tokyo</h1>
+    </header>
 
-        </ul>
+    <?php include "inc/nav.inc"; ?>
+
+  <article>
+    <h2> Hope Barkley | Brief Introduction</h2>
+
+    <figure>
+      <img src="img/portrait.jpg" alt="the picture of Hope Barkley">
+
+      <figcaption>
+        Here is a picture of Hope Barkley
+      </figcaption>
+    </figure>
+
+     <p>
+        Hi, my name is Hope Barkley and I am a digital media studies student at the University of Rochester. I grew up in Tarrytown, NY, a suburb about 30 minutes outside of New York City. I attended Solomon Schechter School of Westchester, a Jewish day school located in Hartsdale, New York. I started gymnastics when I was 7 years-old and was a competetive gymnast throughout my high school career, before deciding not to pursue it in college. Before going to college, I reached USAG level 9 gymnastics and competed on a State and Regional level. During my senior year of high school I applied Early Decision to the University of Rochester and was accepted! I am currently majoring in Digital Media Studies, an interdisciplinary major focussing on technology industries, and hope to pursue a career in design or web development post graduation.
+      </p>
+
+      <div>
+         Please See my initial website! | <a href="http://www.csc174.org/assignment01/hbarkley/"> Hope Barkley | 2018</a>
       </div>
-    </div>
 
-    <?php include "inc/scripts.inc"; ?>
-    
- </body>
+
+
+    <h2> Jonathan Roman | Brief Introduction</h2>
+
+    <figure>
+      <img src="img/jromanpic.jpg" alt="the picture of Jonathan Roman">
+      <figcaption>
+        Here is a picture of Jonathan Roman
+      </figcaption>
+    </figure>
+
+      <p>
+        My name is Jonathan Roman. I'm from Tenafly, NJ and was born September 6th, 1997. I currently attend the University of Rochester with a major in Computer Science. MY hobbies include playing video games and basketball, both of which I participate in groups around campus for.
+      </p>
+
+      <div>
+         Please See my initial website! | <a href="http://www.csc174.org/assignment01/jroman8/"> Jonathan Roman | 2018</a>
+      </div>
+
+
+      <h2> Julia Myers | Brief Introduction</h2>
+        <figure>
+          <img src="img/julia.jpg" alt="the picture of Julia Myers">
+          <figcaption>
+           Here is a picture of Julia Myers
+          </figcaption>
+         </figure>
+
+         <p>
+           I am a junior at the University of Rochester pursuing a major in Digital Media Studies as well as a double minor in Computer Science and Business.  I am originally from Westfield, New Jersey. Use the tabs above to learn more about myself, my courses, and my work experience!
+         </p>
+
+         <div>
+           Please see my initial website! | <a href="http://www.csc174.org/assignment01/jmyers/lab_01/">Julia Myers | 2018</a>
+    </article>
+
+    <footer>
+      <div>
+         This webpage, by team Tokyo
+      </div>
+    </footer>
+
+  </body>
 </html>
