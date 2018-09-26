554dbb39e80889678709b193175e5da9f8972f8e
Valerie Tam <vtam3501@gmail.com>
2018-09-26 16:14:29 -0400

    delete article tags

--- jonathan_roman.php
+++ jonathan_roman.php
@@ -17,7 +17,7 @@
 
 	<?php include "inc/nav.inc"; ?>
 
-	<article>
+	<main>
 
 		<h2>Brief Introduction</h2>
 
@@ -65,7 +65,7 @@
 				My hobbies slightly changed when I arrived to college. I, for the most part, stopped playing tennis and picked up basketball as my favorite sport. Since then I've tried to play nearly every day, and I'm currently the captain of the men's club basketball team here at the University of Rochester. It's been a very different experience to transition from an individually dependent sport such as tennis to a fully organized team sport like basketball. It's been a learning experience and really has helped me grow as a person in regards to my ability to work well with other people / teammates. My interest in gaming remains mostly unchanged, and I'm still keeping up to date with all the changing information in that industry.
 			</p>
 
-	</article>
+	</main>
 
 
 	<footer>
