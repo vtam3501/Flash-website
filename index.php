9277a87d1e032fb02e2e18565834e2d2b27c5ae5
kkao99 <38305127+kkao99@users.noreply.github.com>
2018-09-30 15:45:23 -0400

    add styles to every page

--- index.php
+++ index.php
@@ -10,79 +10,85 @@
 </head>
 
 <body>
-  <header>
-      <h1>The Flash Website of Tokyo</h1>
-  </header>
+    <header>
+        <h1 class="big">The Flash Website of Tokyo</h1>
+    </header>
 
-  <?php include "inc/nav.inc"; ?>
+    <?php include "inc/nav.inc"; ?>
 
-  <main>
+    <main>
 
-    <h2 class="titile"> Hope Barkley | Brief Introduction</h2>
+        <section>
+            <h2> Hope Barkley | Brief Introduction</h2>
 
-      <figure>
-        <img src="images/hbarkley.jpg" alt="the picture of Hope Barkley">
+            <figure class="img-left">
+            <img src="images/hbarkley.jpg" alt="the picture of Hope Barkley">
 
-        <figcaption>
-          Here is a picture of Hope Barkley
-        </figcaption>
-      </figure>
+            <figcaption>
+              Here is a picture of Hope Barkley
+            </figcaption>
+            </figure>
 
-       <p>
-          Hi, my name is Hope Barkley and I am a digital media studies student at the University of Rochester. I am currently majoring in Digital Media Studies, an interdisciplinary major focussing on technology industries, and hope to pursue a career in design or web development post graduation. I grew up in Tarrytown, NY, a suburb about 30 minutes outside of New York City.
-        </p>
+            <p>
+              Hi, my name is Hope Barkley and I am a digital media studies student at the University of Rochester. I am currently majoring in Digital Media Studies, an interdisciplinary major focussing on technology industries, and hope to pursue a career in design or web development post graduation. I grew up in Tarrytown, NY, a suburb about 30 minutes outside of New York City.
+            </p>
 
-        <div>
-           Please see my initial website! | <a href="http://www.csc174.org/assignment01/hbarkley/"> Hope Barkley | 2018</a>
-        </div>
+            <div>
+               Please see my initial website! | <a href="http://www.csc174.org/assignment01/hbarkley/"> Hope Barkley | 2018</a>
+            </div>
 
-    <h2> Julia Myers | Brief Introduction</h2>
+        </section>
 
-        <figure>
+        <section>
+            <h2> Julia Myers | Brief Introduction</h2>
 
-          <img src="images/jmyers.jpg" alt="the picture of Julia Myers">
+            <figure class="img-right">
 
-          <figcaption>
-           Here is a picture of Julia Myers
-          </figcaption>
+              <img src="images/jmyers.jpg" alt="the picture of Julia Myers">
 
-         </figure>
+              <figcaption>
+               Here is a picture of Julia Myers
+              </figcaption>
 
-         <p>
-           Hi, my name is Julia Myers. I am a junior at the University of Rochester pursuing a major in Digital Media Studies as well as a double minor in Computer Science and Business.  I am originally from Westfield, New Jersey.
-         </p>
+             </figure>
 
-         <div>
-           Please see my initial website! | <a href="http://www.csc174.org/assignment01/jmyers/lab_01/">Julia Myers | 2018</a>
-         </div>
+             <p>
+               Hi, my name is Julia Myers. I am a junior at the University of Rochester pursuing a major in Digital Media Studies as well as a double minor in Computer Science and Business.  I am originally from Westfield, New Jersey.
+             </p>
 
-    <h2> Jonathan Roman | Brief Introduction</h2>
+             <div>
+               Please see my initial website! | <a href="http://www.csc174.org/assignment01/jmyers/lab_01/">Julia Myers | 2018</a>
+             </div>
+        </section>
 
-        <figure>
+        <section>
+            <h2> Jonathan Roman | Brief Introduction</h2>
 
-          <img src="images/jroman.jpg" alt="the picture of Jonathan Roman">
+            <figure class="img-left">
 
-          <figcaption>
-            Here is a picture of Jonathan Roman
-          </figcaption>
+            <img src="images/jroman.jpg" alt="the picture of Jonathan Roman">
 
-        </figure>
+            <figcaption>
+                Here is a picture of Jonathan Roman
+            </figcaption>
 
-        <p>
-          Hi, my name is Jonathan Roman. I'm currently attend the University of Rochester with a major in Computer Science, and I'm a senior this semester. I'm from Tenafly, NJ and was born September 6th, 1997.  MY hobbies include playing video games and basketball, both of which I participate in groups around campus for.
-        </p>
+            </figure>
 
-        <div>
-           Please see my initial website! | <a href="http://www.csc174.org/assignment01/jroman8/"> Jonathan Roman | 2018</a>
-        </div>
+            <p>
+              Hi, my name is Jonathan Roman. I'm currently attend the University of Rochester with a major in Computer Science, and I'm a senior this semester. I'm from Tenafly, NJ and was born September 6th, 1997.  MY hobbies include playing video games and basketball, both of which I participate in groups around campus for.
+            </p>
 
+            <div>
+               Please see my initial website! | <a href="http://www.csc174.org/assignment01/jroman8/"> Jonathan Roman | 2018</a>
+            </div>
+        </section>
 
     </main>
 
     <footer>
-      <div>
+        <div class="bottom">
          This webpage, by team Tokyo
-      </div>
+        </div>
     </footer>
 
     <?php include "inc/scripts.inc"; ?>
--- julia_myers.php
+++ julia_myers.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 
 <html lang="en">
 
@@ -12,16 +12,17 @@
 
 <body>
 	<header>
-		<h1>Julia Myers</h1>
+		<h1 class="big">Julia Myers</h1>
 	</header>
 
 	<?php include "inc/nav.inc"; ?>
 
 	<main>
 
-		<h2> Brief Introduction</h2>
+		<section>
+			<h2> Brief Introduction</h2>
 
-			<figure>
+			<figure class="img-left">
 				<img src="images/jmyers.jpg" alt = "Julia Myers">
 				<figcaption>
 					Here is a picture of Julia Myers
@@ -31,18 +32,18 @@
 			<p>
 				Hi, my name is Julia Myers! I am originally from Westfield, New Jersey but I currently live in Rocehster, New York while attending the University of Rochester.  At UofR I am studying to get a major in Digital Media Studies and a double minor in Computer Science and Business. I am also a member of the varsity Cross Country and Track and Field Teams. Outside of classes and athletics, I am a member of the Rochester Design club, the Women in Computing club, and the Varsity Student Athlete Advisory Committee.  I am also a Study Abroad Ambassador.  I have a brother who also attented the University of Rochester, a sister, and a dog.  In my free time I like listening to music, running, and taking pictures.
 			</p>
+		</section>
 
-
-		<h2>Academic Story</h2>
+		<section>
+			<h2>Academic Story</h2>
 
 			<p>
-			 	When I started school at the University of Rochester I started taking a lot od technology and design classes.  For my Digital Media Studies major I have taken courses including Programming for Digital Media, Design in the Digital Age, and Photography which have given me a well-rounded understanding of the media industries and the possible career paths within them.  Additionally, for my Computer Science minor, I have taken courses such as aken at the University of Rochester.  Computer Science classes such as Intro to Programming, Web design and Development, and Recreational Graphics.  For my Business minor I am currently taking Economics and Statistics and will go on to take Finance and Marketing classes in future semesters.
+				When I started school at the University of Rochester I started taking a lot od technology and design classes.  For my Digital Media Studies major I have taken courses including Programming for Digital Media, Design in the Digital Age, and Photography which have given me a well-rounded understanding of the media industries and the possible career paths within them.  Additionally, for my Computer Science minor, I have taken courses such as aken at the University of Rochester.  Computer Science classes such as Intro to Programming, Web design and Development, and Recreational Graphics.  For my Business minor I am currently taking Economics and Statistics and will go on to take Finance and Marketing classes in future semesters.
 			</p>
+		</section>
 
-
-
-		<h2>Experience </h2>
-
+		<section>
+			<h2>Experience </h2>
 			<p>
 				Since I was 16 years old, I have worked as a lifeguard and a swim instructor. I mainly held this job to make money throughout high school and to gain work experience. Also while in high school I worked as a tutor for the National Honors Society.  I tutored English and Math to a freshman student and helped him to study for the SAT.At the end of high school I interned with Nordstrom as a Fashion Ambassador on their BP Fashion Board.  As an intern I helped advise the staff on current trends, make decisions about what should be sold in the teen section of Nordstrom, and promote the brand on social media.
 			</p>
@@ -50,21 +51,22 @@
 			<p>
 				While studying abroad in Copenhagen this past spring, I interned at IIH Nordic, an online marketing and data analytics company.  There, I wrote articles for their website, helped to make promotional videos for the company, and helped to plan their events. I learned more about the possible professions in the technology and marketing fields, and gained practical work experience. Those experiences in conversation with my major courses have made me interested in pursuing an online marketing or data analytics career.
 			</p>
+		</section>
 
+		<section>
+			<h2>One interesting experience I want to share</h2>
 
-		<h2>One interesting experience I want to share</h2>
-			
 			<p>
 				My biggest project during my internship was helping to run their event called Web Analytics Wednesday.  This event gathered data marketers and analysts for a lecture conference where guest lecturers could talk about cool upcoming advancements in the industry.  It was a really cool way to meet influencers and network with some really awesome people!
 			</p>
-
-
-
+		</section>
 
 	</main>
 
 	<footer>
-		Original Source: Julia Myers 2018 | <a href="http://www.csc174.org/assignment01/jmyers/lab_01/"> Initial Website <a>
+		<div class="bottom">
+			Original Source: Julia Myers 2018 | <a href="http://www.csc174.org/assignment01/jmyers/lab_01/"> Initial Website <a>
+		</div>
 	</footer>
 
 	<?php include "inc/scripts.inc"; ?>
